3150481301009679033c5cdce82c747a8cad3312
tomoya <tomoya.y1622@gmail.com>
2024-08-15 23:00:15 +0900

    projectsからprojectへの変更の影響の修正

--- src/components/project/ProjectCard.tsx
+++ src/components/project/ProjectCard.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 export function ProjectCard(props: Props) {
   return (
-    <Link href={`/projects/${props.project.id}`}>
+    <Link href={`/project/${props.project.id}`}>
       <article className='w-52 h-44 shadow p-4 rounded-md'>
         <h1 className='text-xl font-bold py-4'>{props.project.title}</h1>
         <p className='mt-2 max-h-[72px] overflow-hidden overflow-ellipsis'>
